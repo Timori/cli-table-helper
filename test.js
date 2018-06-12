f25efd6e5a2d696b8a2350096ccbb7dff8413801
Timori <toscho@zoho.eu>
2018-06-12 12:03:47 +0200

    addSeperator fixed, makeTable no longer adds seperators

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 import CLITable from './lib/cli-table-helper';
 
-let helpTable = new CLITable(3);
+let table = new CLITable(5);
+
+/*let helpTable = new CLITable(3);
 helpTable.addRow("command", ["params"], "description");
 helpTable.addLine();
 helpTable.addRow("list", null, "List all Users");
@@ -14,4 +16,17 @@ helpTable.addRow("load", ["name|id"], "Load a saved User");
 helpTable.addRow("remove", ["name|id"], "Remove the User off the UserManager");
 helpTable.addRow("save", ["name|id"], "Save the User to a file");
 helpTable.makeTable(true, true);
-helpTable.displayTable();
+helpTable.displayTable();*/
+
+console.log("Banking Account Manager");
+table.addSeperator("-");
+table.addRow("Command:", ["Parameters:"], "Description:");
+table.addLine();
+table.addRow("create", ["Name", "Deposit"], "Creates a new Bank Account.");
+table.addRow("delete", ["Name"], "Deletes the Bank Account by Name.");
+table.addLine();
+table.addRow("deposit", ["Amount"], "Deposit the Amount.");
+table.addRow("withdraw", ["Amount"], "Withdraw the Amount off the Bank Account.");
+table.addSeperator("-");
+table.makeTable(false, false);
+table.displayTable();
